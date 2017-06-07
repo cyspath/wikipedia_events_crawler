632261d0b28bd4871c8673e58f3273506419fafd
Mike <lvlichaelly@gmail.com>
2017-06-07 00:28:12 -0700

    [add] migration for event

--- db/migrate/20170607072318_create_events.rb
+++ db/migrate/20170607072318_create_events.rb
@@ -1,7 +1,10 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-
+      t.date :date, index: true
+      t.string :title, null: false
+      t.text :summary, limit: 128, null: false
+      t.string :image_url
       t.timestamps null: false
     end
   end
